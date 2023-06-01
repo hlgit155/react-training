8bdb1e9aa5a7c54081c81b8ba1173b3bffc7a492
hlgit155 <hlgit155@users.noreply.github.com>
2023-06-01 11:27:50 +1000

    Added women routes & not found page

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,19 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import MainHome from "./pages/Home/MainHome";
-import WomenHome from "./pages/Home/WomenHome";
-import ProductLanding from "./pages/Product/Landing";
+import WomenRoutes from "./routes/WomenRoutes";
+import NotFound from "./pages/NotFound";
 
 function MainLayout() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<MainHome />} />
-        <Route path="/women">
-          <Route index element={<WomenHome />} />
-          <Route path="coats-jackets" element={<ProductLanding />} />
-        </Route>
+        <Route path="/" element={<MainHome/>} />
+        <Route path="/women/*" element={<WomenRoutes/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </>
   );
--- src/routes/WomenRoutes.jsx
+++ src/routes/WomenRoutes.jsx
@@ -1,10 +1,16 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import WomenHome from "./../pages/Home/WomenHome";
+import ProductLanding from "./../pages/Product/Landing";
+import NotFound from "../pages/NotFound";
 
 function WomenRoutes() {
   return (
     <Routes>
-        <Route path="/" element={<WomenRoutes />} />
-      </Routes>
+      <Route index element={<WomenHome />} />
+      <Route path="coats-jackets" element={<ProductLanding />} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
   );
 }
 
