d0c1b5feceb33d6b8ad9777e0ff55a3e4f87b402
hlgit155 <hlgit155@users.noreply.github.com>
2023-06-08 17:04:53 +1000

    Comments on Topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -9,6 +9,13 @@ function Topbar(props) {
   return (
     <TopbarWrap>
       {mainMenu.map((item,index) => {
+        /*-- FEEDBACK --*/
+        // the routeLink should return "item.url" instead of "title", otherewise there will be capital in the url,
+        // it's ok to have capital, but there will be issue when there's other special characters or spaces in title,
+        // that's why we're using object called "url", data should be changed/inserted in the dataset we created
+        // 
+        // ** Another method is to create a convertor function to convert the format from the string of title to acceptable format,
+        // which is more complicated but oftenly necessary for some situtations
         return <TopbarItem onClick={() => {const routeLink = item.title; navigate(routeLink)}} key={index}>{item.title}</TopbarItem>;
       })}
     </TopbarWrap>
