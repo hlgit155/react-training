c4c0af61dd7c09c8d473972081f0a05a42ccb677
blink07min <135078159+blink07min@users.noreply.github.com>
2023-06-09 15:21:10 +1000

    Making menu visible
    There is no errors showing in the terminal but the action is not being implemented. I tried fixing it for a while before committing but cannot do it so could you please check and let me know? Thank you

--- src/pages/Home/WomenHome.jsx
+++ src/pages/Home/WomenHome.jsx
@@ -1,10 +1,63 @@
-function WomenHome() {
-    return (
-      <>
-        <div>Women Home</div>
-      </>
+
+  import React, {useState, useEffect} from 'react';
+  import mainMenu from "../../database/menu/main-menu.json";
+
+  const WomenHome = () => {
+    const [showSubMenu, setShowSubMenu] = useState(false);
+    const [subMenuData, setSubMenuData] = useState([]);
+    const [action, setAction] = useState(null);
+
+    useEffect(() => {
+      const handleMouseEnter = (subMenu) => {
+        setSubMenuData(subMenu);
+        setShowSubMenu(true);
+      };
+      
+      const handleMouseleave = () => {
+        setShowSubMenu(false);
+      };
+
+      if (action === 'women') {
+        handleMouseEnter(mainMenu.navbar.Women);
+      } else if (action === 'kids'){
+        handleMouseEnter(mainMenu.navbar.Kids);
+      } else {
+        handleMouseleave();
+      }
+    }, [action]
     );
-  }
-  
+
+    return (
+      <div>
+        <div>
+          <button onMouseEnter={() => setAction('women')}
+          onMouseLeave={() => setAction(null)}>Women</button>
+          <button onMouseEnter={() => setAction('kids')}
+          onMouseLeave={() => setAction(null)}>Kids</button>
+        </div>
+
+        {
+          showSubMenu && (
+            <div>
+              {subMenuData.map((item,index) => (
+                <div key={index}>
+                  <h3>{item.title}</h3>
+                  <ul>
+                    {item.menuItemps.map((menuItem, index) => (
+                      <li key={index}>
+                        <a href={menuItem.url}>{menuItem.title}</a>
+                      </li>
+                    ))}
+                  </ul>
+                  </div>
+              ))}
+            </div>
+          )
+        }
+      </div>
+    ); 
+
+    
+  };
+
   export default WomenHome;
-  
