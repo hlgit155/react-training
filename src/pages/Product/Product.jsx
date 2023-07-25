a3126d6ffeef146d7bda925221127b7da3275f1d
Yushen Jin <woshijinyushen@gmail.com>
2023-07-25 13:12:51 +1000

    Update product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,13 +1,71 @@
-import React from 'react'
-import dataCoatsAndJackets from '../../database/products/data_coatsAndJackets.json'
-
+import React from "react";
+import dataCoatsAndJackets from "../../database/products/data_coatsAndJackets.json";
+import {
+  ProductWrap,
+  RowProduct,
+  ProductView,
+  GalleryRow,
+  Thumbnails,
+  InnerThumbnails,
+  LargePicture,
+  DescriptionContainer,
+  DescHeader,
+  Sku,
+  DescParagraph,
+  ProductInfo,
+} from "./Product.style";
 
 export default function Product() {
   const data = dataCoatsAndJackets[0];
+
   return (
     <ProductWrap>
-      <ProductView></ProductView>
-      <ProductInfo></ProductInfo>
+      <RowProduct>
+        {/* Left hand side part */}
+        <ProductView>
+          <GalleryRow>
+            <Thumbnails>
+              <InnerThumbnails>
+                {data["product_images"].map((item) => {
+                  return <img src={item.thumbnails} width="100%"></img>;
+                })}
+              </InnerThumbnails>
+            </Thumbnails>
+            <LargePicture>
+              <img
+                src={data["product_images"][0].fullscreen}
+                width="100%"
+              ></img>
+            </LargePicture>
+          </GalleryRow>
+
+          <DescriptionContainer>
+            <DescHeader>Product details</DescHeader>
+            <Sku>{data.sku}</Sku>
+            <DescParagraph
+              dangerouslySetInnerHTML={{ __html: data["short_description"] }}
+            ></DescParagraph>
+          </DescriptionContainer>
+        </ProductView>
+        {/* Right hand side part */}
+        <ProductInfo>
+          <ProductBrand></ProductBrand>
+          <ProductName></ProductName>
+          <ProductPrice></ProductPrice>
+          <ProductColor></ProductColor>
+          <SizeRow>
+            <select></select>
+            <SizeGuide></SizeGuide>
+            
+          </SizeRow>
+          <FindYourBestFit></FindYourBestFit>
+          <ButtonRow>
+            <AddToBag></AddToBag>
+            <WishList></WishList>
+          </ButtonRow>
+          <Material></Material>
+        </ProductInfo>
+      </RowProduct>
     </ProductWrap>
-  )
+  );
 }
--- src/pages/Product/Product.style.jsx
+++ src/pages/Product/Product.style.jsx
@@ -1,6 +1,64 @@
 import styled from "styled-components";
+import { CoreBoxWidth, CoreH1, CoreH2, CoreP } from "../../core/core.styles";
 
 export const ProductWrap = styled(CoreBoxWidth)`
   margin: 0 auto;
   padding: 0 24px;
 `;
+
+export const RowProduct = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  padding: 32px 0;
+`;
+
+export const ProductView = styled.div``;
+
+export const GalleryRow = styled.div`
+  display: flex;
+  width: 100%;
+  aspect-ratio: 17 / 16;
+`;
+
+export const Thumbnails = styled.div`
+  width: 25%;
+  height: 100%;
+  padding-right: 16px;
+`;
+
+export const InnerThumbnails = styled.div`
+  height: 100%;
+  overflow-y: scroll;
+  direction: rtl;
+  padding-left: 8px;
+  width: 100%;
+`;
+
+export const LargePicture = styled.div`
+  width: 100%;
+  padding: 0 16px;
+`;
+
+export const DescriptionContainer = styled.div``;
+
+export const DescHeader = styled(CoreH2)`
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  margin-top: 30px;
+`;
+
+export const Sku = styled(CoreP)`
+  font-size: 12px;
+  line-height: 16px;
+  color: #b0b0b0;
+  margin-bottom: 8px;
+`;
+
+export const DescParagraph = styled(CoreP)`
+  font-size: 12px;
+  line-height: 16px;
+  color: #222;
+  font-weight: 300;
+`;
+export const ProductInfo = styled.div``;
